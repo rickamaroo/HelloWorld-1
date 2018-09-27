8a8c052e8c2e506740e1aa98df00fd0edcca9816
Richard Burns <rburns@wcupa.edu>
2018-09-27 11:16:02 -0400

    Added "HelloWorld"-like code.
    The "public static void main" in Program.cs instantiates a Numbers object. A demo of method call is included.

---
+++ Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class Numbers
+    {
+        int a;
+        int b;
+
+        public Numbers(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+
+        public int add() {
+            return a + b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Numbers n1 = new Numbers(3, 4);
+            Console.WriteLine("3 + 4 = " + n1.add());
+        }
+    }
+}
